c0238355a1b8fd77e356150782d7ccf6a82a2edf
Marc <barnabe.marc@gmail.com>
2015-01-09 17:08:20 +0100

    Basic IO functions wired

--- beads.py
+++ beads.py
@@ -1,5 +1,6 @@
 import pickle
 import os
+import copy
 
 class Produit:
 	id = 0
@@ -37,20 +38,25 @@ class Configuration:
 
 
 def loadConf(string):
-	if conf:
-		c1 = #deepcopy conf
+	if 'conf' in locals():
+		c1 = copy.deepcopy(conf)
 	try:
 		conf=pickle.load(string)
-		if type(conf) not Configuration:
+	except (pickle.UnpicklingError,ValueError) as err :
+		if 'c1' in locals():
 			conf = c1
-			error = "Fichier incompatible"
-	except(#Aucune idée) :
+		error = err
+	if type(conf) != Configuration:
+		err = "Fichier incompatible"
+	del conf
+
+
 
 def saveConf(string):
 	with open(string,rwb) as dump:
 		if os.path.isfile(string):
 			if pickle.load(string).name != conf.name:
-				error = "fichier déjà existant, écraser ?"
+				error = "fichier deja existant, ecraser ?"
 				return
 		pickle.dump(conf, dump)
 
