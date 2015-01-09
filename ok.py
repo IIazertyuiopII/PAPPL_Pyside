c4c55909b4b8075218e7af1004c245c6a3a46d4b
iiazertyuiopii <romain.le-bail@eleves.ec-nantes.fr>
2015-01-09 14:57:00 +0100

    inital commit initial

---
+++ ok.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ok.ui'
+#
+# Created: Fri Jan  9 14:53:56 2015
+#      by: pyside-uic 0.2.15 running on PySide 1.2.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_GroupBox(object):
+    def setupUi(self, GroupBox):
+        GroupBox.setObjectName("GroupBox")
+        GroupBox.resize(400, 300)
+        self.groupBox = QtGui.QGroupBox(GroupBox)
+        self.groupBox.setGeometry(QtCore.QRect(40, 90, 321, 91))
+        self.groupBox.setStyleSheet("")
+        self.groupBox.setObjectName("groupBox")
+        self.label = QtGui.QLabel(self.groupBox)
+        self.label.setGeometry(QtCore.QRect(40, 30, 101, 20))
+        self.label.setObjectName("label")
+        self.label_2 = QtGui.QLabel(self.groupBox)
+        self.label_2.setGeometry(QtCore.QRect(40, 60, 101, 20))
+        self.label_2.setObjectName("label_2")
+        self.lineEdit = QtGui.QLineEdit(self.groupBox)
+        self.lineEdit.setGeometry(QtCore.QRect(160, 30, 113, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtGui.QLineEdit(self.groupBox)
+        self.lineEdit_2.setGeometry(QtCore.QRect(160, 60, 113, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+
+        self.retranslateUi(GroupBox)
+        QtCore.QMetaObject.connectSlotsByName(GroupBox)
+
+    def retranslateUi(self, GroupBox):
+        GroupBox.setWindowTitle(QtGui.QApplication.translate("GroupBox", "GroupBox", None, QtGui.QApplication.UnicodeUTF8))
+        GroupBox.setTitle(QtGui.QApplication.translate("GroupBox", "GroupBox", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox.setTitle(QtGui.QApplication.translate("GroupBox", "Membres", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("GroupBox", "Nombre de jambes", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("GroupBox", "Nombre de bras", None, QtGui.QApplication.UnicodeUTF8))
+        self.lineEdit.setText(QtGui.QApplication.translate("GroupBox", "betete", None, QtGui.QApplication.UnicodeUTF8))
+        self.lineEdit_2.setText(QtGui.QApplication.translate("GroupBox", "bhoufhghg", None, QtGui.QApplication.UnicodeUTF8))
+
